2e98a3dd57742de1a38073cc35abc5dca5b34575
emiel deprost <emiel@n062-06a.wall2.ilabt.iminds.be>
2020-04-03 09:45:14 +0200

    Ensured docker containers are up on restoring

--- restore_backup.sh
+++ restore_backup.sh
@@ -6,6 +6,9 @@ echo "The containers will be restarted afterwards."
 read -r -p "Are you sure you want to continue? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
+	echo "Ensuring containers are up"
+	docker-compose up -d
+    	
 	echo "Restoring data..."
 	docker run --rm --volumes-from influxdb -v $(pwd)/backups/influxdb:/backup ubuntu bash -c "tar xvf /backup/backup.tar"
 	docker run --rm --volumes-from grafana -v $(pwd)/backups/grafana:/backup ubuntu bash -c "tar xvf /backup/backup.tar"
